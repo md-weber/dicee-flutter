2f318accd3e9c84338d56eb6d1ce9ad304bf08f3
myracledesign <weber.max.1988@live.de>
2019-12-05 21:59:40 +0100

    finished the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceImageNumber = 1;
+  int rightDiceImageNumber = 1;
+
+  void rollDice() {
+    setState(() {
+      rightDiceImageNumber = Random().nextInt(6) + 1;
+      leftDiceImageNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                child: Image.asset("images/dice$leftDiceImageNumber.png"),
+                onPressed: () {
+                  rollDice();
+                },
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                child: Image.asset("images/dice$rightDiceImageNumber.png"),
+                onPressed: () {
+                  rollDice();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
